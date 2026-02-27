1a42467a31bdd0138d7a264fa31fa3cb9e1a99db
Vladyslav <vzub210201@gmail.com>
2026-02-27 21:05:29 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,16 @@
-function getElementWidth(content, padding, border) {
-    const contentValue = parseFloat(content);
-    const paddingValue = parseFloat(padding);
-    const borderValue = parseFloat(border);
-    const totalPadding = paddingValue * 2;
-    const totalBorder = borderValue * 2;
-    return contentValue + totalPadding + totalBorder;
+function checkForSpam(message) {
+    const normalizedMessage = message.toLowerCase();
+    if (normalizedMessage.includes("spam") || normalizedMessage.includes("sale")) {
+        return true;
+    } else {
+        return false;
+    }
 }
-getElementWidth();
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
